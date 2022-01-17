96df6e6e8aa911ae4d0abb34664e7d884d04e835
Clifton King <cliftonk@gmail.com>
2022-01-16 20:44:08 -0600

    wip: merkle tree test

--- scripts/merkle-tree.js
+++ scripts/merkle-tree.js
@@ -2,6 +2,14 @@ const { task } = require("hardhat/config");
 const keccak256 = require("keccak256");
 const { MerkleTree } = require("merkletreejs");
 
+const ethers = require("ethers");
+
+function keccak256Address(addr) {
+  return ethers.utils.keccak256(
+    ethers.utils.solidityKeccak256(["address"], [addr])
+  );
+}
+
 task(
   "merkle-tree",
   "generate a merkle tree from a set of space-separated addresses"
@@ -9,7 +17,7 @@ task(
   .addVariadicPositionalParam("addresses")
   .addOptionalParam("claim", "generate a proof for a claimant")
   .setAction(async (taskArgs) => {
-    const leafNodes = taskArgs.addresses.map((addr) => keccak256(addr));
+    const leafNodes = taskArgs.addresses.map((addr) => keccak256Address(addr));
     const tree = new MerkleTree(leafNodes, keccak256, {
       sort: true,
     });
@@ -17,7 +25,7 @@ task(
     console.log(tree.toString());
     console.log(`root ${root}`);
     if (taskArgs.claim) {
-      const leaf = keccak256(taskArgs.claim);
+      const leaf = keccak256Address(taskArgs.claim);
       const proof = tree.getHexProof(leaf);
 
       if (!tree.verify(proof, leaf, root)) {
--- tests/ATXDAONFT_V2.t.sol
+++ tests/ATXDAONFT_V2.t.sol
@@ -3,16 +3,40 @@ pragma solidity ^0.8.9;
 
 import "ds-test/test.sol";
 import "contracts/ATXDAONFT_V2.sol";
+import "hardhat/console.sol";
+import "tests/utils/vm.sol";
 
 contract ATXDAONFTV2Test is DSTest {
+    // see https://github.com/gakonst/foundry/tree/master/forge
+    Vm vm = Vm(0x7109709ECfa91a80626fF3989D68f67F5b1DD12D);
+
     ATXDAONFT_V2 nft;
-    address addrA = address(0x1);
-    address addrB = address(0x2);
-    address addrC = address(0x3);
+    address addrA = address(0x0000000000000000000000000000000000000001);
+    address addrB = address(0x0000000000000000000000000000000000000002);
+    address addrC = address(0x0000000000000000000000000000000000000003);
+
+    bytes32[] proofA = new bytes32[](2);
+
+    bytes32 merkeRootABC =
+        0x16791c28db4bb211d8c63fead28bdf282d7b29e5424daf0add991a8ab0d68204;
 
     function setUp() public {
         nft = new ATXDAONFT_V2();
         nft.initialize("ATX DAO", "ATX");
+
+        proofA[
+            0
+        ] = 0x43678e3b0d0c037990b99215ce550c9478e07cf540f8f843c7b1c9112979496f;
+        proofA[
+            1
+        ] = 0xaa5e58f5c8a07d242c1d1009325e6815281d4aede763b9fb03413c5b2f402b2e;
+    }
+
+    function testMint() public {
+        nft.startMint(1, "uri", merkeRootABC);
+        vm.deal(addrA, 1);
+        vm.prank(addrA);
+        nft.mint{value: 1}(proofA);
     }
 
     function testMintSpecial() public {
