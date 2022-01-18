0aa1c8d611422019eab60b7e1b2a68b62918848b
Clifton King <cliftonk@gmail.com>
2022-01-17 22:20:37 -0600

    add secrets to CI

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,14 +1,15 @@
+import './scripts/merkle-tree';
 import '@nomiclabs/hardhat-ethers';
 import '@nomiclabs/hardhat-etherscan';
 import '@nomiclabs/hardhat-waffle';
 import '@typechain/hardhat';
 import dotenv from 'dotenv';
 import { HardhatUserConfig } from 'hardhat/config';
-import './scripts/merkle-tree';
 
 dotenv.config();
 
-const { API_URL, PRIVATE_KEY, ETHERSCAN_API_KEY } = process.env;
+const { MAINNET_RPC_URL, ROPSTEN_RPC_URL, PRIVATE_KEY, ETHERSCAN_API_KEY } =
+  process.env;
 const config: HardhatUserConfig = {
   solidity: {
     version: '0.8.9',
@@ -25,11 +26,11 @@ const config: HardhatUserConfig = {
   networks: {
     hardhat: {},
     ropsten: {
-      url: API_URL,
+      url: ROPSTEN_RPC_URL,
       accounts: [`0x${PRIVATE_KEY}`],
     },
     mainnet: {
-      url: API_URL,
+      url: MAINNET_RPC_URL,
       accounts: [`0x${PRIVATE_KEY}`],
     },
   },
