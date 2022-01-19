6c0c710e0e4ee0fc8799931c2ca18dbab8d405bc
Clifton King <cliftonk@gmail.com>
2022-01-19 00:38:48 -0600

    add start/end to get-nft-owners

--- scripts/get-nft-owners.ts
+++ scripts/get-nft-owners.ts
@@ -4,6 +4,8 @@ import { task } from 'hardhat/config';
 
 interface GetNftOwnersArgs {
   contract: ContractName;
+  start: number;
+  end: number;
 }
 
 task('get-nft-owners', 'gets a list of nft owners, ordered by token id')
@@ -11,24 +13,26 @@ task('get-nft-owners', 'gets a list of nft owners, ordered by token id')
     'contract',
     'contract name (e.g. ATXDAONFT or ATXDAONFT_V2)'
   )
-  .setAction(async ({ contract }: GetNftOwnersArgs, { ethers, network }) => {
-    const contractAddress = getContractAddress(contract, network.name);
-    const nft = (await ethers.getContractAt(
-      contract,
-      contractAddress
-    )) as ATXDAONFT;
-    let id = 1;
-    const owners: string[] = [];
-    while (true) {
-      try {
-        const owner = await nft.ownerOf(id);
-        owners.push(owner);
-        ++id;
-      } catch {
-        break;
+  .addOptionalParam<number>('start', 'token id to start')
+  .addOptionalParam<number>('end', 'token id to end (inclusive)')
+  .setAction(
+    async ({ contract, start, end }: GetNftOwnersArgs, { ethers, network }) => {
+      const contractAddress = getContractAddress(contract, network.name);
+      const nft = (await ethers.getContractAt(
+        contract,
+        contractAddress
+      )) as ATXDAONFT;
+      const owners: string[] = [];
+      for (let id = start || 1; id <= end || 99999999; id += 1) {
+        try {
+          const owner = await nft.ownerOf(id);
+          owners.push(owner);
+        } catch {
+          break;
+        }
       }
-    }
 
-    console.error(`${owners.length} found!`);
-    console.log(JSON.stringify(owners, null, 4));
-  });
+      console.error(`${owners.length} found!`);
+      console.log(JSON.stringify(owners, null, 4));
+    }
+  );
