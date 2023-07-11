5222eab3ddb758a65e7ce5130ec7ecc27861032b
Clifton King <cliftonk@gmail.com>
2023-07-11 15:43:36 -0500

    Bluebonnet scripts (#32)
    * stage 1 script to split into seen groups
    
    * image downloader working
    
    * master json manifest ready
    
    * fixed typescript compile
    
    * eslint is a mess
    
    * scripts done

---
+++ scripts/bluebonnet-01-fetch-minters.ts
@@ -0,0 +1,141 @@
+import { readFileSync } from 'fs';
+import { globSync } from 'fast-glob';
+import { google } from 'googleapis';
+import { task } from 'hardhat/config';
+import { ATXDAONFT_V2 } from '../typechain-types';
+import { getContractAddress } from '../util/contract-meta';
+import {
+  getAddress,
+  arrayify,
+  toUtf8Bytes,
+  concat,
+  keccak256,
+} from 'ethers/lib/utils';
+
+interface MinterInput {
+  addressOrEns: string;
+  imageUrl: string;
+  isNewMember: boolean;
+}
+
+interface MinterData {
+  address: string;
+  imageUrl: string;
+  isNewMember: boolean;
+  imageHash: string;
+}
+
+async function getWorksheetData(
+  sheetName: string,
+  isNewMember: boolean
+): Promise<MinterInput[]> {
+  const auth = new google.auth.GoogleAuth({
+    credentials: JSON.parse(
+      readFileSync('keys/atxdao-service-account.json', 'utf8')
+    ),
+    // Scopes can be specified either as an array or as a single, space-delimited string.
+    scopes: ['https://www.googleapis.com/auth/spreadsheets'],
+  });
+  const gsapi = google.sheets({ version: 'v4', auth });
+  const data = (
+    await gsapi.spreadsheets.values.get({
+      spreadsheetId: '1PWOJ_pU5ac4puCjxrtNaKEuppPTaE3nI22vhkHDpiAE', // replace with your spreadsheet ID
+      range: `${sheetName}!A2:M500`,
+      valueRenderOption: 'UNFORMATTED_VALUE',
+    })
+  ).data.values;
+
+  if (!data) {
+    throw new Error('no data found');
+  }
+
+  return data
+    .filter((row) => row && row[3])
+    .map(
+      (row): MinterInput => ({
+        addressOrEns: row[3],
+        imageUrl: row[4],
+        isNewMember,
+      })
+    );
+}
+
+const getImageHash = (address: string, imageUrl: string): string => {
+  const addressBytes = arrayify(getAddress(address));
+  const imageBytes = toUtf8Bytes(imageUrl);
+  const concatenated = concat([addressBytes, imageBytes]);
+  return keccak256(concatenated).slice(-6);
+};
+
+task(
+  'bluebonnet-01-fetch-minters',
+  'fetch bluebonnet metadata from google sheets and generate metadata groups'
+).setAction(async ({}, { ethers, network }): Promise<void> => {
+  let previousMinters: MinterData[] = [];
+  globSync('metadata/bluebonnet/groups/*.json').forEach((path) => {
+    try {
+      const group = JSON.parse(readFileSync(path, 'utf8'));
+      previousMinters = [...previousMinters, ...group];
+    } catch (e) {}
+  });
+  console.error(`found ${previousMinters.length} existing minters`);
+  const minterMap = previousMinters.reduce(
+    (acc: Record<string, MinterData>, minter) => {
+      const { address } = minter;
+      const checksummedAddress = getAddress(address);
+      acc[checksummedAddress] = minter;
+      return acc;
+    },
+    {}
+  );
+
+  const networkName = network.name === 'hardhat' ? 'mainnet' : network.name;
+
+  const nftv2 = (await ethers.getContractAt(
+    'ATXDAONFT_V2',
+    getContractAddress('ATXDAONFT_V2', networkName)
+  )) as ATXDAONFT_V2;
+
+  const memberData = [
+    ...(await getWorksheetData('New Members', true)),
+    ...(await getWorksheetData('Trade in', false)),
+  ];
+
+  const seenAddresses = new Set<string>();
+
+  const resolvedMembers = await Promise.all(
+    memberData.map(async ({ addressOrEns, imageUrl, isNewMember }) => {
+      const address = await ethers.provider.resolveName(addressOrEns);
+      if (!address) {
+        throw new Error(`could not resolve ${addressOrEns} for ${imageUrl}`);
+      }
+      if (seenAddresses.has(address)) {
+        throw new Error(
+          `duplicate address ${address} (${addressOrEns}) for ${imageUrl}`
+        );
+      }
+      seenAddresses.add(address);
+      const nftBalance = await nftv2.balanceOf(address);
+      if (isNewMember && !nftBalance.eq(0)) {
+        throw new Error(`${address} (${addressOrEns}) is already a member`);
+      }
+      if (!isNewMember && nftBalance.eq(0)) {
+        throw new Error(
+          `${address} (${addressOrEns}) does not own a membership NFT`
+        );
+      }
+      return {
+        address,
+        imageUrl: imageUrl,
+        isNewMember: isNewMember,
+        imageHash: getImageHash(address, imageUrl),
+      };
+    })
+  );
+
+  const newMinters = resolvedMembers.filter(
+    ({ address }) => !minterMap[address]
+  );
+
+  console.log(JSON.stringify(newMinters, null, 2));
+});
---
+++ scripts/bluebonnet-02-download-images.ts
@@ -0,0 +1,85 @@
+import { readFileSync, createWriteStream, unlink, existsSync } from 'fs';
+import https from 'https';
+import { globSync } from 'fast-glob';
+import { task } from 'hardhat/config';
+import { mkdirpSync } from 'mkdirp';
+
+interface MinterData {
+  address: string;
+  imageUrl: string;
+  isNewMember: boolean;
+  imageHash: string;
+}
+
+export interface ErrnoException extends Error {
+  errno?: number;
+  code?: string;
+  path?: string;
+  syscall?: string;
+  stack?: string;
+}
+
+const downloadImage = (imageUrl: string, dest: string): Promise<void> =>
+  new Promise((resolve, reject) => {
+    if (existsSync(dest)) {
+      console.error(`File ${dest} already exists`);
+      resolve();
+      return;
+    }
+    console.error(`downloading ${imageUrl} to ${dest}`);
+    const file = createWriteStream(dest, { flags: 'wx' });
+
+    https.get(imageUrl, (response) => {
+      if (response.statusCode !== 200) {
+        reject(
+          new Error(`Failed to get '${imageUrl}' (${response.statusCode})`)
+        );
+        return;
+      }
+
+      response.pipe(file);
+    });
+
+    file.on('finish', () => resolve());
+    file.on('error', (error: ErrnoException) => {
+      file.close();
+      if (error.code === 'EEXIST') reject(new Error('File already exists'));
+      else unlink(dest, () => reject(error));
+    });
+  });
+
+task(
+  'bluebonnet-02-download-images',
+  'from metadata groups, download images'
+).setAction(async ({}, {}): Promise<void> => {
+  const groups = globSync('metadata/bluebonnet/groups/*.json').map((path) => {
+    const groupName = path.split('/').pop()?.replace('.json', '');
+    if (!groupName) {
+      throw new Error(`invalid path ${path}`);
+    }
+    const minters: MinterData[] = JSON.parse(readFileSync(path, 'utf8'));
+    return { groupName, minters };
+  });
+
+  // check for duplicate image hashes
+  groups.forEach(({ minters }) => {
+    const seenHashes: Set<string> = new Set();
+    minters.forEach(({ imageHash, address }) => {
+      if (seenHashes.has(imageHash)) {
+        throw new Error(`duplicate image hash ${imageHash} for ${address}`);
+      }
+      seenHashes.add(imageHash);
+    });
+  });
+
+  for (const { groupName, minters } of groups) {
+    const imagePath = `metadata/bluebonnet/images/${groupName}`;
+    await Promise.all(
+      minters.map(async ({ imageHash, imageUrl }) => {
+        const imageDest = `${imagePath}/${imageHash}.png`;
+        mkdirpSync(imagePath);
+        await downloadImage(imageUrl, imageDest);
+      })
+    );
+  }
+});
